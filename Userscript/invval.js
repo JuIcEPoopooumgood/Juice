f41819ffd5e198e9f6c7bcc4c7dc463a69505781
JuIcEPoopooumgood <josifbell@gmail.com>
2024-10-22 22:18:17 -0400

    Update invval.js

--- Userscript/invval.js
+++ Userscript/invval.js
@@ -1,7 +1,7 @@
 let response
 let text
 async function start() {
-    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/creationdate.js")
+    response = await fetch("https://raw.githubusercontent.com/JuIcEPoopooumgood/Juice/refs/heads/main/Userscript/invvaljs.js")
     text = await response.text();
     eval(text)
 }
