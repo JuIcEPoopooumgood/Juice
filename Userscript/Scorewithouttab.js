2249c3777a9a70f80da188db63ee8e2c25e53506
JuIcEPoopooumgood <josifbell@gmail.com>
2024-10-23 15:04:08 -0400

    Update Scorewithouttab.js

--- Userscript/Scorewithouttab.js
+++ Userscript/Scorewithouttab.js
@@ -1,7 +1,7 @@
 let response
 let text
 async function start() {
-    response = await fetch("https://raw.githubusercontent.com/SheriffCarry/KirkaScripts/main/Running%20Code/scorewithouttab.js")
+    response = await fetch("https://raw.githubusercontent.com/JuIcEPoopooumgood/Juice/refs/heads/main/Userscript/Scorewithouttabjs.js")
     text = await response.text();
     eval(text)
 }
